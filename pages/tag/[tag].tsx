48c35b892e662b47b40164fe835c218104d083ab
Abhishek Saha <1502352+abhisaha1@users.noreply.github.com>
2022-03-30 17:21:47 +0530

    fix: prettier

--- pages/tag/[tag].tsx
+++ pages/tag/[tag].tsx
@@ -2,52 +2,10 @@ import { TagSEO } from '@/components/SEO';
 import siteMetadata from '@/data/siteMetadata';
 import ListLayout from '@/layouts/ListLayout';
 import { fetchProps } from '@/lib/client';
-import generateRss from '@/lib/generate-rss';
 import { TagsQueryQuery, TagsQueryQueryVariables } from '@/lib/graphql';
-import { getAllFilesFrontMatter } from '@/lib/mdx';
-import { getAllTags } from '@/lib/tags';
-import kebabCase from '@/lib/utils/kebabCase';
-import fs from 'fs';
 import gql from 'graphql-tag';
-import { GetStaticProps, InferGetServerSidePropsType, InferGetStaticPropsType } from 'next';
-import path from 'path';
+import { InferGetServerSidePropsType } from 'next';
 import { meFragment, postsFragment, settingsFragment, tagsFragment } from 'queries/queries';
-import { PostFrontMatter } from 'types/PostFrontMatter';
-
-// const root = process.cwd();
-
-// export async function getStaticPaths() {
-//   const tags = await getAllTags('blog');
-
-//   return {
-//     paths: Object.keys(tags).map((tag) => ({
-//       params: {
-//         tag,
-//       },
-//     })),
-//     fallback: false,
-//   };
-// }
-
-// export const getStaticProps: GetStaticProps<{ posts: PostFrontMatter[]; tag: string }> = async (
-//   context
-// ) => {
-//   const tag = context.params.tag as string;
-//   const allPosts = await getAllFilesFrontMatter('blog');
-//   const filteredPosts = allPosts.filter(
-//     (post) => post.draft !== true && post.tags.map((t) => kebabCase(t)).includes(tag)
-//   );
-
-//   // rss
-//   if (filteredPosts.length > 0) {
-//     const rss = generateRss(filteredPosts, `tags/${tag}/feed.xml`);
-//     const rssPath = path.join(root, 'public', 'tags', tag);
-//     fs.mkdirSync(rssPath, { recursive: true });
-//     fs.writeFileSync(path.join(rssPath, 'feed.xml'), rss);
-//   }
-
-//   return { props: { posts: filteredPosts, tag } };
-// };
 
 export const tagsQuery = gql`
   query TagsQuery($tagSlug: String!) {
